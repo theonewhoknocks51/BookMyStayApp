a032a5d86fa1c0a78442ffa68f62fec2170305eb
unknown <ghoshtitas26@gmail.com>
2026-03-20 14:51:21 +0530

    Use CASE 1

---
+++ App/src/UseCase1HotelBookingApp.java
@@ -0,0 +1,26 @@
+/**
+ * UseCase1HotelBookingApp
+ *
+ * This class represents the entry point of the Hotel Booking Management System.
+ * It demonstrates how a Java application starts and displays a welcome message.
+ *
+ * @author TITAS
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("=======================================");
+        System.out.println(" Welcome to Book My Stay Application ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("=======================================");
+
+        // Application ends
+    }
+}
